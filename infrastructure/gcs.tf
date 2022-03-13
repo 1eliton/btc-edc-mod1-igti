9179fd497b7957b1df4b0d8c252bd7dfb04ffcda
1eliton <elituhh@gmail.com>
2022-03-13 10:18:44 -0300

    .

--- infrastructure/gcs.tf
+++ infrastructure/gcs.tf
@@ -1,7 +1,7 @@
 
 
 # Create a GCS Bucket -- raw data
-resource "google_storage_bucket" "enem_staging" {
+resource "google_storage_bucket" "poc_igti_enem_raw_data" {
     name = "raw_data"
     location = var.region
 }
