f6e43695732fb7a0db7ae0d5ece2c9d670448111
1eliton <elituhh@gmail.com>
2022-03-13 11:07:38 -0300

    add resource

--- infrastructure/gcs.tf
+++ infrastructure/gcs.tf
@@ -5,3 +5,9 @@ resource "google_storage_bucket" "poc_igti_btc_edc_enem_raw_data_001" {
     name = "poc_igti_btc_edc_enem_raw_data_001"
     location = var.region
 }
+
+# Create a GCS Bucket -- delta table
+resource "google_storage_bucket" "poc_igti_btc_edc_enem_delta_table_001" {
+    name = "poc_igti_btc_edc_enem_delta_table_001"
+    location = var.region
+}
