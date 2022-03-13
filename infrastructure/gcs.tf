b9ba5f1ce298174206f39840015732c09fe909c0
1eliton <elituhh@gmail.com>
2022-03-13 10:23:21 -0300

    fix name

--- infrastructure/gcs.tf
+++ infrastructure/gcs.tf
@@ -2,6 +2,6 @@
 
 # Create a GCS Bucket -- raw data
 resource "google_storage_bucket" "poc_igti_btc_edc_enem_raw_data_001" {
-    name = "raw_data"
+    name = "poc_igti_btc_edc_enem_raw_data_001"
     location = var.region
 }
