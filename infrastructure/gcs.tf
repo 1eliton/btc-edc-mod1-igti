8b5b300ab1c5e03f2bd41a537a32417e52e417df
Eliton Gadotti <eliton@outlook.com>
2022-03-14 22:09:33 -0300

    added notes

--- infrastructure/gcs.tf
+++ infrastructure/gcs.tf
@@ -2,7 +2,9 @@
 # SHOULD REVIEW
 # https://towardsdatascience.com/deploy-cloud-functions-on-gcp-with-terraform-111a1c4a9a88
 # https://console.cloud.google.com/home/activity?project=poc-gcp-340811
-
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/dataproc_cluster
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/dataproc_job
+# https://gist.github.com/neil90/51a8fb2d96f2552f84234ab576f48eb2
 # Create a GCS Bucket -- raw data
 resource "google_storage_bucket" "poc_igti_btc_edc_enem_raw_data_001" {
     name = "poc_igti_btc_edc_enem_raw_data_001"
